3a01304b432583546361c6afc73870a7f86a3963
Sebas <seplarui@gmail.com>
2018-08-24 21:04:20 +0200

    User Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   users = ['ryan', 'joe', 'cameron', 'john', 'bruce'];
+  activated = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaMundoComponent
+    HolaMundoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,

807bf3995594f2f8eea816eac4486abf6a518f1a
Sebas <seplarui@gmail.com>
2018-08-24 20:15:03 +0200

    Componente Generado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorld } from './primercomponente/primer.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorld,
+    HolaMundoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
